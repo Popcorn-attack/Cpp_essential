d44f9226b0ecaefd93d637b0909731852b51ab5a
Cody <shuqiqi66@gmail.com>
2024-04-01 21:26:15 +0100

    00

--- codes/procedural_programming/refclass.cpp
+++ codes/procedural_programming/refclass.cpp
@@ -2,7 +2,30 @@
 #include<stdio.h>
 using namespace std;
 
-int main(void){
 
-    cout<<"c++"<<endl;
+bool fibon_elem(int pose,int &elem)
+{
+    if (pose<0||pose>1000)
+    { elem = 0; return false;    }
+    elem = 1;
+    int n1 =1, n2 =1;
+    for (int i =1;i<=pose;i++)
+    {
+        elem = n2+n1;
+        n2 = n1;
+        n1 = elem;
+    }
+    return true;
+}
+
+
+
+int main(void){
+    int pos;
+    cout<<"input a position"<<endl;
+    cin>>pos;
+
+    int elem;
+    if (fibon_elem(pos,elem))
+    cout<<elem<<endl;
 }
